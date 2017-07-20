6d341c0bfee389ca8015ff062ca9db32443af76a
Matthew Lein <matthewlein@hotmail.com>
2017-07-20 15:01:30 -0500

    flatten out controls to avoid a lot of props passing for no reason

--- src/index.js
+++ src/index.js
@@ -10,122 +10,6 @@ import RotationInput from './components/RotationInput'
 import ColorPicker from './components/ColorPicker'
 import Header from './components/Header'
 
-class Controls extends React.Component {
-  getStripeClasses(stripe, idx) {
-    const classes = ['stripes__item'];
-    if (idx === this.props.currentStripeIdx) {
-      classes.push('stripes__item--active');
-    }
-    if (!this.props.stripes[idx].visible) {
-      classes.push('stripes__item--hidden');
-    }
-    return classes.join(' ');
-  }
-
-  render() {
-    const stripes = this.props.stripes.map((stripe, idx) => {
-      return (
-        <li
-          key={stripe.id}
-          data-idx={idx}
-          onClick={(e) => this.props.onStripeClick(e)}
-          className={this.getStripeClasses(stripe, idx)}
-        >
-          <label className="stripe__visible-label">
-            <input className="stripe__visible-input" type="checkbox" name="visible" checked={stripe.visible} onChange={(e) => this.props.onInputChange(e, idx)}/>
-            <div className="stripe__visible-box"></div>
-          </label>
-          <div className="stripes__item-color" style={{backgroundColor: stripe.color}}></div>
-          <button className="stripes__item-delete" type="button" data-idx={idx} onClick={(e) => this.props.removeStripe(e, idx)}>⨉</button>
-        </li>
-      )
-    })
-
-    return (
-      <section className="controls">
-        <section className="stripe-settings">
-
-          <div className="flex col-2">
-            <div className="flex__col-1_2">
-              <RotationInput
-                value={this.props.currentStripe.rotation}
-                onRotationChange={(e) => this.props.onRotationChange(e)}
-                updateRotation={(e) => this.props.updateRotation(e)}
-              />
-            </div>
-            <div className="flex__col-1_2">
-              <ColorPicker
-                name='color'
-                value={this.props.currentStripe.color}
-                onChange={(e) => this.props.onInputChange(e, this.props.currentStripeIdx)}
-                updateColor={(name, color) => this.props.updateColor(name, color, this.props.currentStripeIdx)}
-              />
-              <label className="plaid__label controls__label" title="Stripes go vertically and horizontally">
-                Plaid
-                <input className="plaid__check" type="checkbox" name="plaid" checked={this.props.currentStripe.plaid} onChange={(e) => this.props.onInputChange(e, this.props.currentStripeIdx)} />
-              </label>
-            </div>
-          </div>
-
-          <RangeInput
-            name="opacity"
-            value={this.props.currentStripe.opacity}
-            min={0}
-            max={100}
-            onRangeChange={(e) => this.props.onRangeChange(e)}
-          />
-          <RangeInput
-            name="width"
-            value={this.props.currentStripe.width}
-            min={1}
-            max={200}
-            onRangeChange={(e) => this.props.onRangeChange(e)}
-          />
-          <RangeInput
-            name="gap"
-            value={this.props.currentStripe.gap}
-            min={1}
-            max={200}
-            onRangeChange={(e) => this.props.onRangeChange(e)}
-          />
-          <RangeInput
-            name="offset"
-            value={this.props.currentStripe.offset}
-            min={0}
-            max={200}
-            onRangeChange={(e) => this.props.onRangeChange(e)}
-          />
-
-        </section>
-
-        <section className="stripes">
-          <button className="stripes__new-button controls__button" type="button" onClick={() => this.props.onNewStripe()}>＋New Stripe</button>
-          <Sortable
-            tag="ol"
-            className="stripes__list"
-            onChange={(order, sortable, evt) => this.props.onStripeOrderChange(order, sortable, evt)}
-          >
-            {stripes}
-          </Sortable>
-        </section>
-
-        <section className="background-settings">
-          <ColorPicker
-            name="Background Color"
-            value={this.props.bg}
-            top={true}
-            onChange={(e) => this.props.updateBackgroundColor(e)}
-            updateColor={(name, color) => this.props.updateBackgroundColor(null, color)}
-          />
-        </section>
-
-        {}
-
-      </section>
-    )
-  }
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -134,6 +18,17 @@ class App extends React.Component {
       bg: this.props.pattern.bg,
       currentStripeIdx: 0
     };
+  }
+
+  getStripeClasses(stripe, idx) {
+    const classes = ['stripes__item'];
+    if (idx === this.state.currentStripeIdx) {
+      classes.push('stripes__item--active');
+    }
+    if (!this.state.stripes[idx].visible) {
+      classes.push('stripes__item--hidden');
+    }
+    return classes.join(' ');
   }
 
   onStripeOrderChange(stripes, sortable, event) {
@@ -258,26 +153,108 @@ class App extends React.Component {
   }
 
   render() {
+    const stripes = this.state.stripes.map((stripe, idx) => {
+      return (
+        <li
+          key={stripe.id}
+          data-idx={idx}
+          onClick={(e) => this.onStripeClick(e)}
+          className={this.getStripeClasses(stripe, idx)}
+        >
+          <label className="stripe__visible-label">
+            <input className="stripe__visible-input" type="checkbox" name="visible" checked={stripe.visible} onChange={(e) => this.onInputChange(e, idx)}/>
+            <div className="stripe__visible-box"></div>
+          </label>
+          <div className="stripes__item-color" style={{backgroundColor: stripe.color}}></div>
+          <button className="stripes__item-delete" type="button" data-idx={idx} onClick={(e) => this.removeStripe(e, idx)}>⨉</button>
+        </li>
+      )
+    })
+    const currentStripe = this.state.stripes[this.state.currentStripeIdx];
+
     return (
       <div className="app">
         <Header/>
         <main className="main">
-          <Controls
-            bg={this.state.bg}
-            stripes={this.state.stripes}
-            currentStripe={this.state.stripes[this.state.currentStripeIdx]}
-            currentStripeIdx={this.state.currentStripeIdx}
-            onStripeOrderChange={this.onStripeOrderChange.bind(this)}
-            onStripeClick={this.onStripeClick.bind(this)}
-            removeStripe={this.removeStripe.bind(this)}
-            onInputChange={this.onInputChange.bind(this)}
-            onRangeChange={this.onRangeChange.bind(this)}
-            onNewStripe={this.onNewStripe.bind(this)}
-            onRotationChange={this.onRotationChange.bind(this)}
-            updateRotation={this.updateRotation.bind(this)}
-            updateColor={this.updateColor.bind(this)}
-            updateBackgroundColor={this.updateBackgroundColor.bind(this)}
-          />
+          <section className="controls">
+            <section className="stripe-settings">
+
+              <div className="flex col-2">
+                <div className="flex__col-1_2">
+                  <RotationInput
+                    value={currentStripe.rotation}
+                    onRotationChange={(e) => this.onRotationChange(e)}
+                    updateRotation={(e) => this.updateRotation(e)}
+                  />
+                </div>
+                <div className="flex__col-1_2">
+                  <ColorPicker
+                    name='color'
+                    value={currentStripe.color}
+                    onChange={(e) => this.onInputChange(e, this.state.currentStripeIdx)}
+                    updateColor={(name, color) => this.updateColor(name, color, this.state.currentStripeIdx)}
+                  />
+                  <label className="plaid__label controls__label" title="Stripes go vertically and horizontally">
+                    Plaid
+                    <input className="plaid__check" type="checkbox" name="plaid" checked={currentStripe.plaid} onChange={(e) => this.onInputChange(e, this.state.currentStripeIdx)} />
+                  </label>
+                </div>
+              </div>
+
+              <RangeInput
+                name="opacity"
+                value={currentStripe.opacity}
+                min={0}
+                max={100}
+                onRangeChange={(e) => this.onRangeChange(e)}
+              />
+              <RangeInput
+                name="width"
+                value={currentStripe.width}
+                min={1}
+                max={200}
+                onRangeChange={(e) => this.onRangeChange(e)}
+              />
+              <RangeInput
+                name="gap"
+                value={currentStripe.gap}
+                min={1}
+                max={200}
+                onRangeChange={(e) => this.onRangeChange(e)}
+              />
+              <RangeInput
+                name="offset"
+                value={currentStripe.offset}
+                min={0}
+                max={200}
+                onRangeChange={(e) => this.onRangeChange(e)}
+              />
+
+            </section>
+
+            <section className="stripes">
+              <button className="stripes__new-button controls__button" type="button" onClick={() => this.onNewStripe()}>＋New Stripe</button>
+              <Sortable
+                tag="ol"
+                className="stripes__list"
+                onChange={(order, sortable, evt) => this.onStripeOrderChange(order, sortable, evt)}
+              >
+                {stripes}
+              </Sortable>
+            </section>
+
+            <section className="background-settings">
+              <ColorPicker
+                name="Background Color"
+                value={this.state.bg}
+                top={true}
+                onChange={(e) => this.updateBackgroundColor(e)}
+                updateColor={(name, color) => this.updateBackgroundColor(null, color)}
+              />
+            </section>
+
+          </section>
+
           <PatternizerPreview patternData={this.stateFiltered()} />
         </main>
       </div>
