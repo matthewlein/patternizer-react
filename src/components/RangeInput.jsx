1749de7ebab45a650265889e34196812ed3ffb7f
Matthew Lein <matthewlein@hotmail.com>
2017-07-24 14:19:16 -0500

    making range input functional component

--- src/components/RangeInput.jsx
+++ src/components/RangeInput.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 
-export default class RangeInput extends React.Component {
-  render() {
-    return (
-      <label className="controls__label">
-        <span className="controls__label-text">{this.props.name}</span>
-        <div className="controls__range">
-          <input className="range" value={this.props.value} name={this.props.name} type="range" onInput={(event) => this.props.onRangeChange(event)} onChange={(event) => this.props.onRangeChange(event)} min={this.props.min} max={this.props.max} />
-        </div>
-        <div className="controls__range-input-wrapper">
-          <input className="controls__range-input" value={this.props.value} name={this.props.name} type="number" onChange={(event) => this.props.onRangeChange(event)} min={this.props.min} max={this.props.max} />
-        </div>
-      </label>
-    )
-  }
+const RangeInput = ({
+  name,
+  value,
+  min,
+  max,
+  onRangeChange
+}) => {
+  return (
+    <label className="controls__label">
+      <span className="controls__label-text">{name}</span>
+      <div className="controls__range">
+        <input
+          type="range"
+          className="range"
+          name={name}
+          value={value}
+          min={min}
+          max={max}
+          onInput={(event) => onRangeChange(event)}
+          onChange={(event) => onRangeChange(event)}
+        />
+      </div>
+      <div className="controls__range-input-wrapper">
+        <input
+          type="number"
+          className="controls__range-input"
+          name={name}
+          value={value}
+          min={min}
+          max={max}
+          onChange={(event) => onRangeChange(event)}
+        />
+      </div>
+    </label>
+  )
 }
+
+export default RangeInput;
